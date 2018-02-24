af352d2524eeeb4f8e8df16e176d24afce75b3fe
BaddDog <bboychuk@shaw.ca>
2018-02-24 14:04:17 -0800

    Major additions 2018-02-24

--- MainActivity.java
+++ MainActivity.java
@@ -579,6 +579,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return "";
         }
 
+        
         @Override
         protected void onPostExecute(String token) {
             // Change title once completed
